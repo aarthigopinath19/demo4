a82a5f13830516289a8222a9caf0900d68ae1e20
aarthigopinath19 <aarthiravi19@gmail.com>
2023-04-12 11:07:55 +0530

    change 1

--- src/task4/demotask.java
+++ src/task4/demotask.java
@@ -4,6 +4,8 @@ public class demotask {
 	
 		public static void main(String[] args) {
 			System.out.println("Demo 1"); 
+			System.out.println("Demo 2");
+			System.out.println("Demo 3");
 			
 		}
 
