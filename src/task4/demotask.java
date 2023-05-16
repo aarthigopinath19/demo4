db713d4fdd719f468844feaae4bc687ade3241bb
aarthigopinath19 <aarthiravi19@gmail.com>
2023-05-16 17:37:05 +0530

    new commit

--- src/task4/demotask.java
+++ src/task4/demotask.java
@@ -6,7 +6,8 @@ public class demotask {
 			System.out.println("Demo 1"); 
 			System.out.println("Demo 2");
 			System.out.println("Demo 3");
-			
+			System.out.println("Demo 4");
+			System.out.println("Demo 5");
 		}
 
 
